3ec5e998d2371f6c3e4af3f456a08c92cea4d3ec
Brian Zanti <brianzanti@gmail.com>
2020-07-27 14:48:28 -0600

    add nav bar test

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -3,35 +3,31 @@ require 'rails_helper'
 
 RSpec.describe 'Site Navigation' do
   describe 'As a Visitor' do
-    it "I see a nav bar with links to all pages" do
-      visit '/merchants'
-
-      within 'nav' do
-        click_link 'All Items'
-      end
-
-      expect(current_path).to eq('/items')
-
-      within 'nav' do
-        click_link 'All Merchants'
-      end
-
-      expect(current_path).to eq('/merchants')
-    end
-
     it "I can see a cart indicator on all pages" do
       visit '/merchants'
 
       within 'nav' do
-        expect(page).to have_content("Cart: 0")
+        expect(page).to have_link("Cart: 0")
       end
 
       visit '/items'
 
       within 'nav' do
-        expect(page).to have_content("Cart: 0")
+        expect(page).to have_link("Cart: 0")
       end
+    end
 
+    it "Has a nav bar with links to all pages" do
+      visit '/'
+
+      within '#navigation' do
+        expect(page).to have_link('Monstar Shop')
+        expect(page).to have_link('Items')
+        expect(page).to have_link('Merchants')
+        expect(page).to have_link('Login')
+        expect(page).to have_link('Register')
+        expect(page).to have_link('Cart: 0')
+      end
     end
   end
 end
