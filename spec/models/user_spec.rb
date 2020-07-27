6fb6cc272243564004319eb531af5f89e9bad205
Brian Zanti <brianzanti@gmail.com>
2020-07-27 15:35:49 -0600

    adds user factory to spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe User do
 
   describe 'roles' do
     it 'is default by default' do
-      user = User.create!
+      user = create(:user)
       expect(user.role).to eq("default")
       expect(user.default?).to be(true)
       expect(user.admin?).to be(false)
@@ -21,7 +21,7 @@ RSpec.describe User do
     end
 
     it 'can be a merchant admin' do
-      user = User.create!(role: :merchant_admin)
+      user = create(:user, role: :merchant_admin)
       expect(user.role).to eq("merchant_admin")
       expect(user.merchant_admin?).to be(true)
       expect(user.default?).to be(false)
@@ -29,7 +29,7 @@ RSpec.describe User do
     end
 
     it 'can be an admin' do
-      user = User.create!(role: :admin)
+      user = create(:user, role: :admin)
       expect(user.role).to eq("admin")
       expect(user.admin?).to be(true)
       expect(user.default?).to be(false)
