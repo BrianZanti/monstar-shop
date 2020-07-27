6b3f49ea5aedc1a6d17601852cb611cd982920d3
Brian Zanti <brianzanti@gmail.com>
2020-07-27 15:44:01 -0600

    adds user navigation spec

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -30,4 +30,34 @@ RSpec.describe 'Site Navigation' do
       end
     end
   end
+
+  describe 'as a Logged in User' do
+    before :each do
+      @user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    it "shows a message that the user is logged in" do
+      visit '/'
+
+      within '#navigation' do
+        expect(page).to have_content("Logged In as #{@user.name}")
+      end
+    end
+
+    it "has all visitor links, plus a profile and logout, minus login and register" do
+      visit '/'
+
+      within '#navigation' do
+        expect(page).to have_link('Monstar Shop')
+        expect(page).to have_link('Items')
+        expect(page).to have_link('Merchants')
+        expect(page).to have_link('Log Out')
+        expect(page).to have_link('Cart: 0')
+        expect(page).to have_link('Profile')
+        expect(page).to_not have_link('Login')
+        expect(page).to_not have_link('Register')
+      end
+    end
+  end
 end
