360d59a82272cfcbe65b0510a98ad63314129072
Brian Zanti <brianzanti@gmail.com>
2020-07-28 13:22:56 -0600

    redirects roles to correct paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     log_in(user)
     flash[:success] = "Welcome #{user.name}, you are now logged in."
-    redirect_to profile_path
+    redirect_to profile_path if user.default?
+    redirect_to merchant_path if user.merchant_employee?
+    redirect_to admin_path if user.admin?
   end
 end
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User Login' do
   describe 'as a user' do
-    it 'can login with valid info' do
+    it 'can login with valid info and redirect to profile page' do
       user = create(:user)
 
       visit '/'
@@ -15,6 +15,42 @@ RSpec.describe 'User Login' do
       click_button 'Login'
 
       expect(current_path).to eq(profile_path)
+      expect(page).to have_content("Welcome #{user.name}, you are now logged in.")
+    end
+  end
+
+  describe 'as an Admin' do
+    it 'can login with valid info and redirect to the Merchant Dashboard' do
+      user = create(:admin)
+
+      visit '/'
+      click_link 'Login'
+
+      expect(current_path).to eq(login_path)
+
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_button 'Login'
+
+      expect(current_path).to eq(admin_path)
+      expect(page).to have_content("Welcome #{user.name}, you are now logged in.")
+    end
+  end
+
+  describe 'as a Merchant Employee' do
+    it 'can login with valid info and redirect to the Merchant Dashboard' do
+      user = create(:merchant_employee)
+
+      visit '/'
+      click_link 'Login'
+
+      expect(current_path).to eq(login_path)
+
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_button 'Login'
+
+      expect(current_path).to eq(merchant_path)
       expect(page).to have_content("Welcome #{user.name}, you are now logged in.")
     end
   end
